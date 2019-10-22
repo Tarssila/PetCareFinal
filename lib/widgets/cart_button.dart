e8cfed98c900337cf570d97213e4831674956340
Tarssila <tarssila2014@gmail.com>
2019-10-22 15:45:25 -0400

    Pet Care Final

---
+++ lib/widgets/cart_button.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:loja_virtual/widgets/edit_category_dialog.dart';
+
+class CartButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      child: Icon(Icons.add, color: Colors.white,),
+      onPressed: (){
+        showDialog(context: context,
+          builder: (context) => EditCategoryDialog()
+
+        );
+      },
+    );
+  }
+}
